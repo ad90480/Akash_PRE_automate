c52122442ec872c6df143f27321fd902bc73daa7
Akash Dwivedi <ad90480@gmail.com>
2023-09-14 16:22:52 +0530

    Merge pull request #2 from ad90480/first_branch
    First branch_1

---
+++ src/main/java/com/previnci/pages/CommonlyUsed.java
@@ -0,0 +1,27 @@
+package com.previnci.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class CommonlyUsed extends BasePage {
+    public CommonlyUsed(WebDriver driver) {
+        super(driver);
+    }
+    @FindBy(how = How.XPATH,using = "//previnci-buttons[@ng-reflect-label=\"Submit\"]")
+    private WebElement Submitbutton;
+
+    @FindBy(how = How.XPATH,using = "//previnci-buttons[@ng-reflect-label=\"Cancel\"]")
+    private WebElement Cancelbutton;
+
+    public void submitbutton() {
+        super.click(this.Submitbutton);
+
+    }
+
+    public void cancelbutton() {
+        super.click(this.Cancelbutton);
+
+    }
+}
